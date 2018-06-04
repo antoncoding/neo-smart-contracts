8f3c7c85dd1039e49a76dbef146d491b181ad9fe
Anton Cheng <b03705027@ntu.edu.tw>
2018-06-04 22:22:32 +0800

    Update with Util.py to have independent key generate function instead of hardcode everytime

--- NeonAD.py
+++ NeonAD.py
@@ -5,6 +5,7 @@ from boa.builtins import concat
 from boa.interop.Neo.Blockchain import GetHeader, GetHeight
 from boa.interop.Neo.Header import GetTimestamp
 
+from NeonAdUtil import *
 
 def get_current_timestamp():
     current_height = GetHeight()
@@ -20,45 +21,92 @@ def get_ad_count():
         return ad_registered
     return 0
 
+
+def get_default_content():
+    default_content_key = "NeonAD.default"
+    default_content = Get(GetContext(), default_content_key)
+    return default_content
+
+
+def update_board_round(board_id):
+    owner_key = get_owner_key(board_id)
+    content_key = get_content_key(board_id)
+    endtime_key = get_endtime_key(board_id)
+
+    highest_bidder_key = get_highest_bidder_key(board_id)
+    highest_bid_key = get_highest_bid_key(board_id)
+    next_content_key = get_next_content_key(board_id)
+
+    period = Get(GetContext(), get_period_key(board_id))
+    current_timestamp = get_current_timestamp()
+    round_end = current_timestamp + period
+
+    # update Info
+    Put(GetContext(), endtime_key, round_end)
+
+    new_owner = Get(GetContext(), highest_bidder_key)
+    Put(GetContext(), owner_key, new_owner)
+
+    new_content = Get(GetContext(), next_content_key)
+    Put(GetContext(), content_key, new_content)
+
+    Put(GetContext(), highest_bid_key, 0)
+
+    print('Update Round Completed')
+
+
 def init_board_info(board_id, creator, period, domain_name):
     # Define All the keys we need for a single board
-    highest_actual_bid_key = concat(board_id, ".real")
-    highest_mask_bid_key = concat(board_id, ".mask")
-    period_key = concat(board_id, ".period")
-    ad_owner_key = concat(board_id, ".owner")
-    ad_bidder_key = concat(board_id, ".bidder")
-    domain_name_key = concat(board_id, ".domain")
-    content_key = concat(board_id, ".content")
-
-    # Records for next Round
-    endtime_key = concat(board_id, ".endtime")
-    next_bidder_key = concat(board_id, ".bidder.next")
-    next_content_key = concat(board_id, ".content.next")
-
-    current_timestamp = get_current_timestamp()
-    first_round_end = current_timestamp + period
+    period_key = get_period_key(board_id)
+    ad_admin_key = get_ad_admin_key(board_id)
+    domain_name_key = get_domain_key(board_id)
 
     Put(GetContext(), period_key, period)
-    Put(GetContext(), endtime_key, first_round_end)
-    Put(GetContext(), ad_owner_key, creator)
-    Put(GetContext(), ad_bidder_key, creator)
+    Put(GetContext(), ad_admin_key, creator)
     Put(GetContext(), domain_name_key, domain_name)
-    Put(GetContext(), content_key, '')
+
+    # Records for next Round
+    highest_bid_key = get_highest_bid_key(board_id)
+    highest_bidder_key = get_highest_bidder_key(board_id)
+    next_content_key = get_next_content_key(board_id)
+
     # Next Round
-    Put(GetContext(), highest_actual_bid_key, 0)
-    Put(GetContext(), highest_mask_bid_key, 0)
-    Put(GetContext(), next_bidder_key, creator)
-    Put(GetContext(), next_content_key, '')
+    Put(GetContext(), highest_bid_key, 0)
+    Put(GetContext(), highest_bidder_key, creator)
+    default_content = get_default_content()
+    Put(GetContext(), next_content_key, default_content)
+
+
+
+def check_expired(board_id):
+    current_timestamp = get_current_timestamp()
+    board_end_key = get_endtime_key(board_id)
+    board_end_timestamp = Get(GetContext(), board_end_key)
+    if board_end_timestamp > current_timestamp:
+        return False
     return True
 
 
+def bid_for_board(board_id, bidder, bid, content):
+    if bid <= 0:
+        ''' and other value checking'''
+        return False
 
+    highest_bid_key = get_highest_bid_key(board_id)
+    highest_bid = Get(GetContext(), highest_bid_key)
+    if bid > highest_bid:
+        highest_bidder_key = get_highest_bidder_key(board_id)
+        Put(GetContext(), highest_bidder_key, bidder)
 
+        Put(GetContext(), highest_bid_key, bid)
 
-def check_validat(board_id):
-    current_timestamp = get_current_timestamp
-    board_end_key = concat(board_id, ".endtime")
-    board_end_timestap = Get(GetContext(), board_end_key)
+        next_content_key = get_next_content_key(board_id)
+        Put(GetContext(), next_content_key, content)
+        return True
+
+    else:
+        print('Bid Smaller than Current Bid')
+        return False
 
 
 def Main(operation, args):
@@ -66,15 +114,10 @@ def Main(operation, args):
     Main definition for the smart contracts
 
     :param operation: the operation to be performed
-    :type operation: str
-
     :param args: list of arguments.
         args[0] is always sender script hash
-    :param type: str
-
-    :return:
-        byterarray: The result of the operation
     """
+    contract_owner = b'#\xba\'\x03\xc52c\xe8\xd6\xe5"\xdc2 39\xdc\xd8\xee\xe9'
     user_hash = args[0]
 
     if operation == "GetEndTime":
@@ -93,44 +136,47 @@ def Main(operation, args):
         args[1] is domain name
         args[2] is bid round (second)
         """
-        # parameters
         domain_name = args[1]
         period = args[2]
 
-        # Create New Board ID
         ad_count = get_ad_count() + 1
         board_id = concat("NeonAD", ad_count)
         Put(GetContext(), "NeonAD.count", ad_count)
 
-        success = init_board_info(board_id, user_hash, period, domain_name)
+        init_board_info(board_id, user_hash, period, domain_name)
+        update_board_round(board_id)
 
         return board_id
 
-    # This doesn't require authentication
-    # if operation == 'GetDomain':
-    #     return Get(GetContext(), domain_target_key)
+
+    elif operation == "BidForBoard":
+        """
+        args[1] := board ID
+        args[2] := Bid (NEP)
+        args[3] := Content
+        """
+
+        board_id = args[1]
+        bid = args[2]
+
+        expired = check_expired(board_id)
+        if expired:
+            update_board_round()
+            print('Going into next Round')
+
+        content = args[3]
+        if bid_for_board(board_id, user_hash, bid, content) == True:
+            return 'Bid Placed'
+        else:
+            return 'Bid Failed'
 
 
-    # if operation == 'RegisterDomain':
-    #     if(CheckWitness(owner)):
-    #         address = args[2]
-    #         Put(GetContext(), domain_owner_key, address)
-    #         if len(args) == 4:
-    #             target = args[3]
-    #             Put(GetContext(), domain_target_key, target)
-    #         return True
-    #
-    # if operation == 'SetDomainTarget':
-    #     if CheckWitness(domain_owner_key) or CheckWitness(owner):
-    #         # License the product
-    #         target = args[2]
-    #         Put(GetContext(), domain_target_key, target)
-    #         return True
-    #
-    # if operation == 'DeleteDomain':
-    #     if(CheckWitness(owner)):
-    #         Delete(GetContext(), domain_owner_key)
-    #         Delete(GetContext(), domain_target_key)
-    #         return True
+    elif operation == "SetDefaultContent":
+        if CheckWitness(contract_owner):
+            ad_content = args[1]
+            Put(GetContext(), "NeonAD.default", ad_content)
+            return ad_content
+        else:
+            print('This Function can only be triggered by admin')
 
-    return False
+    return 'Failed'
