d6d12013c563c21424ce2078745b63ac14c25d02
Anton Cheng <b03705027@ntu.edu.tw>
2018-06-06 16:02:05 +0800

    storage example

---
+++ sample-storage.py
@@ -0,0 +1,15 @@
+from boa.interop.Neo.Storage import GetContext, Put, Delete, Get
+from boa.builtins import concat
+
+def Main():
+    address = 'ARbpKynS2E2dBBrTnAfMjXgf7vsFv7PmZa'
+    balance_key = concat(address, '_balance') # ARbpKynS2E2dBBrTnAfMjXgf7vsFv7PmZa_balance
+    # Get from Storage
+    balance = Get(GetContext(), balance_key)
+    if not balance:
+        balance = 0
+
+    new_balance = balance + 100
+    Put(GetContext(), balance_key, new_balance)
+
+    return True
