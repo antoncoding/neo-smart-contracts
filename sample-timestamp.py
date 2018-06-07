0eca2c8ffd65de3c7b616707e2c11c0b9020290d
Anton Cheng <b03705027@ntu.edu.tw>
2018-06-07 21:50:55 +0800

    sample update, with GetTime method

--- sample-timestamp.py
+++ sample-timestamp.py
@@ -1,8 +1,11 @@
 from boa.interop.Neo.Blockchain import GetHeader, GetHeight
-from boa.interop.Neo.Header import GetTimestamp
+from boa.interop.Neo.Runtime import GetTime
 
 def Main():
     current_height = GetHeight()
     currentBlock = GetHeader(current_height)
     time = currentBlock.Timestamp
+
+    # # Or just use GetTime Function
+    # time = GetTime()
     return time
